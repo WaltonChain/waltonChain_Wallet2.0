6dd0f6f5958a2903e91f7a6a2babe831d4893389
abiaqiou <abiaqiou@gmail.com>
2022-09-08 11:46:19 +0800

    remove unused import

--- lib/app/modules/token_detail/controllers/token_detail_controller.dart
+++ lib/app/modules/token_detail/controllers/token_detail_controller.dart
@@ -1,7 +1,6 @@
 // import 'package:flutter/material.dart';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart';
